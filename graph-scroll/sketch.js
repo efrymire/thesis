e09fcb63e227ad88a828721f9f4b2cd8e2aae440
Ellie Frymire <fryme970@newschool.edu>
2018-03-08 18:46:42 +0000

    react

--- graph-scroll/sketch.js
+++ graph-scroll/sketch.js
@@ -137,36 +137,36 @@ function render(){
 
     // ------- SVG 2 --------
 
-    var svg2 = d3.select('.container-2 #graph').html('')
-        .append('svg')
-        .attrs({width: width, height: height})
+    // var svg2 = d3.select('.container-2 #graph').html('')
+    //     .append('svg')
+    //     .attrs({width: width, height: height})
 
-    var path = svg2.append('path')
+    // var path = svg2.append('path')
 
     // ------- SCROLL 2 --------
 
-    var gs2 = d3.graphScroll()
-        .container(d3.select('.container-2'))
-        .graph(d3.selectAll('.container-2 #graph'))
-        .eventId('uniqueId2')  // namespace for scroll and resize events
-        .sections(d3.selectAll('.container-2 #sections > div'))
-        .on('active', function(i){
-            var h = height
-            var w = width
-            var dArray = [
-                [[w/4, h/4], [w*3/4, h/4],  [w*3/4, h*3/4], [w/4, h*3/4]],
-                [[0, 0],     [w*3/4, h/4],  [w*3/4, h*3/4], [w/4, h*3/4]],
-                [[w/2, h/2], [w, h/4],      [w, h],         [w/4, h]],
-                [[w/2, h/2], [w, h/4],      [w, h],         [w/4, h]],
-                [[w/2, h/2], [w, h/2],      [0, 0],         [w/4, h/2]],
-                [[w/2, h/2], [0, h/4],      [0, h/2],         [w/4, 0]],
-            ].map(function(d){ return 'M' + d.join(' L ') })
+    // var gs2 = d3.graphScroll()
+    //     .container(d3.select('.container-2'))
+    //     .graph(d3.selectAll('.container-2 #graph'))
+    //     .eventId('uniqueId2')  // namespace for scroll and resize events
+    //     .sections(d3.selectAll('.container-2 #sections > div'))
+    //     .on('active', function(i){
+    //         var h = height
+    //         var w = width
+    //         var dArray = [
+    //             [[w/4, h/4], [w*3/4, h/4],  [w*3/4, h*3/4], [w/4, h*3/4]],
+    //             [[0, 0],     [w*3/4, h/4],  [w*3/4, h*3/4], [w/4, h*3/4]],
+    //             [[w/2, h/2], [w, h/4],      [w, h],         [w/4, h]],
+    //             [[w/2, h/2], [w, h/4],      [w, h],         [w/4, h]],
+    //             [[w/2, h/2], [w, h/2],      [0, 0],         [w/4, h/2]],
+    //             [[w/2, h/2], [0, h/4],      [0, h/2],         [w/4, 0]],
+    //         ].map(function(d){ return 'M' + d.join(' L ') })
 
 
-            path.transition().duration(1000)
-                .attr('d', dArray[i])
-                .style('fill', colors[i])
-        })
+    //         path.transition().duration(1000)
+    //             .attr('d', dArray[i])
+    //             .style('fill', colors[i])
+        // })
 
     d3.select('#source')
         .styles({'margin-bottom': window.innerHeight - 450 + 'px', padding: '100px'})
