fa6f6d5bc804ad0689b2eb933aa1a3f2bf5b70ca
efrymire <fryme970@newschool.edu>
2018-04-14 13:22:58 -0400

    new test sketch, beginning real data

--- graph-scroll/sketch.js
+++ graph-scroll/sketch.js
@@ -25,23 +25,21 @@ function render(){
 
     // data function
 
-    d3.csv('test-data.csv', function(data) {
+    d3.csv('date_count', function(data) {
         var dataset = data;
 
         dataset.forEach(function(d) {
             d.date = Date.parse(d.date);
-            d.tweets = parseInt(d.tweets);
-            d.pos = parseInt(d.pos);
-            d.neg = parseInt(d.neg);
+            d.count = parseInt(d.count);
         })
 
         var x = d3.scaleBand()
                 .range([0, width])
                 .padding(0.1)
                 .domain(dataset.map(function(d) { return d.date; })),
-            y = d3.scaleLinear()
-                .range([0, height/2])
-                .domain([0, d3.max(dataset, function(d) { return d.tweets; })]);
+            y = d3.scaleLog()
+                .range([0, height])
+                .domain([1000, d3.max(dataset, function(d) { return d.count; })]);
 
         var rect = svg.selectAll('rect')
             .data(dataset)
@@ -49,20 +47,22 @@ function render(){
             .append('rect')
             .attr("class", "bar")
             .attr('x', function(d) { return x(d.date) })
-            .attr('y', function(d) { return height/2 - (y(d.tweets)) })
+            .attr('y', function(d) { return height - (y(d.count)) })
             .attr('width', x.bandwidth())
-            .attr('height', function(d) { return y(d.tweets) })
+            .attr('height', function(d) { return y(d.count) })
+            .attr('date', function(d) { return (d.date) })
+            .attr('count', function(d) { return (d.count) })
             .on("mouseover", mouseover)
             .on("mousemove", mousemove)
             .on("mouseout", mouseout);
 
         var text = svg.selectAll('rect')
             .append('text')
-            .text(function(d) { return d.tweets; })
+            .text(function(d) { return d.count; })
             .style('fill','black')
             .attr('x', function(d) { return x(d.date) })
-            .attr('y', function(d) { return height - (y(d.tweets)) })
-            .attr('transform', function(d, i) { return 'translate(10, ' + (window.innerHeight-y(d.tweets)) + ')rotate(-90)'; });
+            .attr('y', function(d) { return height - (y(d.count)) })
+            .attr('transform', function(d, i) { return 'translate(10, ' + (window.innerHeight-y(d.count)) + ')rotate(-90)'; });
 
 
         var div = d3.select('body').append('div')
@@ -75,9 +75,9 @@ function render(){
 
         function mousemove() {
             div
-                .text("text")
-                .style("left", (d3.event.pageX - 34) + "px")
-                .style("top", (d3.event.pageY - 12) + "px");
+                .text('date:' + d3.select(this).attr('date') + ', tweets:' + d3.select(this).attr('count'))
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 25) + "px");
         }
 
         function mouseout() {
@@ -92,80 +92,69 @@ function render(){
             .graph(d3.selectAll('container-1 #graph'))
             .eventId('uniqueId1')  // namespace for scroll and resize events
             .sections(d3.selectAll('.container-1 #sections > div'))
-            // .offset(innerWidth < 900 ? innerHeight - 30 : 200)
-            .on('active', function(i){
-
-                var ypos = [
-                    function(d) { return height/2 - (y(d.tweets)) },
-                    function(d) { return height/2 - (y(d.pos)) },
-                    function(d) { return height - x(d.date) },
-                    height/2
-                ];
-
-                var xpos = [
-                    function(d) { return x(d.date) },
-                    function(d) { return x(d.date) },
-                    function(d) { return width/2 - (y(d.pos)) },
-                    0
-                ];
-
-                var wid = [
-                    x.bandwidth(),
-                    x.bandwidth(),
-                    function(d) { return y(d.tweets) },
-                    width
-                ];
-
-                var hgt = [
-                    function(d) { return y(d.tweets) },
-                    function(d) { return y(d.tweets) },
-                    x.bandwidth(),
-                    10
-                ];
-
-                rect.transition().duration(600)
-                    .attr('y', ypos[i] )
-                    .attr('x', xpos[i] )
-                    .attr('width', wid[i] )
-                    .attr('height', hgt[i] )
-                    .transition()
-                    .style('fill', colors[i]);
-
-            })
+            .offset(innerWidth < 900 ? innerHeight - 30 : 200)
 
     });
 
     // ------- SVG 2 --------
 
-    // var svg2 = d3.select('.container-2 #graph').html('')
-    //     .append('svg')
-    //     .attrs({width: width, height: height})
+    var svg2 = d3.select('.container-2 #graph').html('')
+        .append('svg')
+        .attrs({width: width, height: height})
 
-    // var path = svg2.append('path')
+    d3.csv('date_count', function(data) {
+        var dataset = data;
+
+        dataset.forEach(function (d) {
+            d.date = Date.parse(d.date);
+            d.count = parseInt(d.count);
+        })
+
+        var x = d3.scaleBand()
+                .range([0, width])
+                .padding(0.1)
+                .domain(dataset.map(function (d) {
+                    return d.date;
+                })),
+            y = d3.scaleLinear()
+                .range([0, height])
+                .domain([0, d3.max(dataset, function (d) {
+                    return d.count;
+                })]);
+
+        var tweet = svg2.selectAll('circle')
+            .data(dataset)
+            .enter()
+            .append('circle')
+            .attr('r', '2')
+            .attr('cx', function(d) { return x(d.date)})
+            .attr('cy', height)
+
+    })
 
     // ------- SCROLL 2 --------
 
-    // var gs2 = d3.graphScroll()
-    //     .container(d3.select('.container-2'))
-    //     .graph(d3.selectAll('.container-2 #graph'))
-    //     .eventId('uniqueId2')  // namespace for scroll and resize events
-    //     .sections(d3.selectAll('.container-2 #sections > div'))
-    //     .on('active', function(i){
-    //         var h = height
-    //         var w = width
-    //         var dArray = [
-    //             [[w/4, h/4], [w*3/4, h/4],  [w*3/4, h*3/4], [w/4, h*3/4]],
-    //             [[0, 0],     [w*3/4, h/4],  [w*3/4, h*3/4], [w/4, h*3/4]],
-    //             [[w/2, h/2], [w, h/4],      [w, h],         [w/4, h]],
-    //             [[w/2, h/2], [w, h/4],      [w, h],         [w/4, h]],
-    //             [[w/2, h/2], [w, h/2],      [0, 0],         [w/4, h/2]],
-    //             [[w/2, h/2], [0, h/4],      [0, h/2],         [w/4, 0]],
-    //         ].map(function(d){ return 'M' + d.join(' L ') })
+    var gs2 = d3.graphScroll()
+        .container(d3.select('.container-2'))
+        .graph(d3.selectAll('.container-2 #graph'))
+        .eventId('uniqueId2')  // namespace for scroll and resize events
+        .sections(d3.selectAll('.container-2 #sections > div'))
+        // .on('active', function(i){
+            // var h = height
+            // var w = width
+            // var dArray = [
+            //     [[w/4, h/4], [w*3/4, h/4],  [w*3/4, h*3/4], [w/4, h*3/4]],
+            //     [[0, 0],     [w*3/4, h/4],  [w*3/4, h*3/4], [w/4, h*3/4]],
+            //     [[w/2, h/2], [w, h/4],      [w, h],         [w/4, h]],
+            //     [[w/2, h/2], [w, h/4],      [w, h],         [w/4, h]],
+            //     [[w/2, h/2], [w, h/2],      [0, 0],         [w/4, h/2]],
+            //     [[w/2, h/2], [0, h/4],      [0, h/2],         [w/4, 0]],
+            // ].map(function(d){ return 'M' + d.join(' L ') })
 
 
-    //         path.transition().duration(1000)
-    //             .attr('d', dArray[i])
-    //             .style('fill', colors[i])
+        //     tweet.transition().duration(1000)
+        //         .attr('d', dArray[i])
+        //         .style('fill', colors[i])
         // })
 
     d3.select('#source')
