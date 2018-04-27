63d95ea40f01146fbaedd3a6043810f9582425ce
efrymire <fryme970@newschool.edu>
2018-04-27 13:00:09 -0400

    updates prior to revamp

--- sandbox/sketch-test.js
+++ sandbox/sketch-test.js
@@ -1,61 +1,96 @@
-var width = window.innerWidth,
-    height = window.innerHeight
+var data = [];
+d3.range(100).forEach(function(el) {
+    data.push({ value: el });
+});
 
-d3.select('body').append('svg')
+console.log(data)
+
+var width = window.innerHeight,
+    height = window.innerWidth
+
+var canvas = d3.select('.container-3')
+    .append('canvas')
     .attr('width', width)
     .attr('height', height)
 
-// Prepare our physical space
-var g = d3.select('svg').append('g');
+var context = canvas.node().getContext('2d');
 
-// Get the data from our CSV file
-d3.csv('packing', function(error, data) {
-    if (error) throw error;
+// This is your SVG replacement and the parent of all other elements
+var customBase = document.createElement('custom');
+var custom = d3.select(customBase);
 
-    console.log(data)
+// Settings for a grid with 10 cells in a row,
+// 100 cells in a block and 1000 cells in a row.
+var groupSpacing = 4;
+var cellSpacing = 2;
+var offsetTop = height / 5;
+var cellSize = Math.floor((width - 11 * groupSpacing) / 100) - cellSpacing;
 
-    stratified = d3.stratify()(data);
-    console.log(stratified)
+function databind(data) {
+    // Bind data to custom elements.
 
-    data.forEach(function(d) {
-        d.parentId = parseInt(d.parentId)
-        d.likes = parseInt(d.likes);
-    })
+    // Get a scale for the colours - not essential but nice.
+    // colourScale = d3.scaleSequential(d3.interpolateSpectral)
+    //     .domain(d3.extent(data, function(d) { return d; }));
 
-    var l = d3.scaleLinear()
-            .range([3,13])
-            .domain([0, d3.max(data, function(d) { return d.likes; })]);
+    var join = custom.selectAll('custom.rect')
+        .data(data);
+
+    var enterSel = join
+        .enter()
+        .append('custom')
+        .attr('class', 'rect')
+        .attr("x", function(d, i) {
+            var x0 = Math.floor(i / 100) % 10, x1 = Math.floor(i % 10);
+            return groupSpacing * x0 + (cellSpacing + cellSize) * (x1 + x0 * 10); })
+        .attr("y", function(d, i) {
+            var y0 = Math.floor(i / 1000), y1 = Math.floor(i % 100 / 10);
+            return groupSpacing * y0 + (cellSpacing + cellSize) * (y1 + y0 * 10); })
+        .attr('width', 0)
+        .attr('height', 0);
+
+    join
+        .merge(enterSel)
+        .transition()
+        .attr('width', cellSize)
+        .attr('height', cellSize)
+        // .attr('fillStyle', function(d) { return colourScale(d); });
+        .attr('fillStyle', 'rgb(50, 50, 50)');
+
+    var exitSel = join.exit()
+        .transition()
+        .attr('width', 0)
+        .attr('height', 0)
+        .remove();
+
+    console.log()
+
+}
 
 
-    // ------- circle packing --------
+function draw() {
+    // Draw the elements on the canvas.
 
-    // Declare d3 layout
-    var layout = d3.pack()
-        .size([width, height])
-        .padding(5)
+    context.clearRect(0, 0, width, height); // Clear the canvas.
 
-    // Layout + Data
-    var root = d3.hierarchy(stratified).sum(function (d) { return (d.data.likes); });
-    var nodes = root.descendants();
+    // Draw each individual custom element with their properties.
+    var elements = custom.selectAll('custom.rect');
+    console.log(elements)
 
-    console.log(nodes)
+    // Grab all elements you bound data to in the databind() function.
+    elements.each(function(d,i) { // For each virtual/custom element...
 
-    layout(root);
+        var node = d3.select(this);
+        // This is each individual element in the loop.
 
-    var tweet = g.selectAll('circle').data(nodes).enter().append('circle');
+        context.fillStyle = node.attr('fillStyle');
+        // Here you retrieve the colour from the individual in-memory node and set the fillStyle for the canvas paint
 
-    // ------- draw random --------
+        context.fillRect(node.attr('x'), node.attr('y'), node.attr('width'), node.attr('height'));
+        // Here you retrieve the position of the node and apply it to the fillRect context function which will fill and paint the square.
+    }); // Loop through each element.
 
-    // tweet.attr('r', function(d) { return (d.r)})
-    //     .attr('cx',function() { return (Math.random() * width)})
-    //     .attr('cy', function() { return (Math.random() * height)})
-    //     .style('opacity',0.3)
+}
 
-    // ------- draw packing --------
-
-    tweet.attr('cx', function (d) { return d.x; })
-        .attr('cy', function (d) { return d.y; })
-        .attr('r', function (d) { return (d.r); })
-        .style('opacity','0.3')
-
-});
+databind(data);
+draw();
